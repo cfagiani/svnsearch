39b30657efb6828496108fbaec2cb57fc92eec02
Christopher Fagiani <christopher.fagiani@gmail.com>
2013-08-23 12:41:29 -0400

    use argparser to handle command line

--- README.md
+++ README.md
@@ -9,16 +9,19 @@ This simple script will search the files in an HTTP-hosted subversion repository
 The search can be restricted to a comma-delimited set of file extensions and can be set up to start from a specific repository link.
 
 ##Usage
-		python svnsearch.py <baseurl> <username> <password> <fileFilter> <searchString> [startLink]
-where:
-**baseurl** is the starting url for the repository
-**username** is the username to use for the http auth challenge
-**password** is the password to use for the http auth challenge
-**fileFilter** is a comma-delimited list of file extensions to check (i.e. *java,xml* )
-**startLink**  (optional) is the link from which to start the search (as it appear in the html on the page located at baseUrl)
+		python svnsearch.py [-h] [-start link] [-u username] [-p password] baseUrl fileFilter searchString
+
+positional arguments:
+  baseUrl       Fully qualified URL of root
+  fileFilter    Comma-delimited list of file extensions to search
+  searchString  String for which to search
+
+optional arguments:
+  -h, --help    show this help message and exit
+  -start link   link on the page at baseUrl from which to start the search
+  -u username   SVN Username
+  -p password   SVN Password
 
 
 ##TODO:
-- Clean up command line arguments
-- Make auth optional
 - Multithread
--- svnsearch.py
+++ svnsearch.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python
-import sys
+import argparse
 
 from svncrawler import SvnCrawler
 
-def checkargs():
-  if len(sys.argv) < 6:
-    print 'Bad command.\nUsage: svnsearch <baseurl> <username> <password> <fileFilter> <searchString> [startLink]'
-    sys.exit(1)
+def handleArgs():
+    argparser = argparse.ArgumentParser(description="Search a Subversion repository over HTTP.")
+    argparser.add_argument('baseUrl',metavar='baseUrl', help='Fully qualified URL of root')
+    argparser.add_argument('fileFilter', metavar='fileFilter', help='Comma-delimited list of file extensions to search')
+    argparser.add_argument('searchString',metavar='searchString', help='String for which to search')
+    argparser.add_argument('-start', metavar='link', help='link on the page at baseUrl from which to start the search', dest='start')
+    argparser.add_argument('-u', metavar='username', help='SVN Username', dest='user')
+    argparser.add_argument('-p', metavar='password',help='SVN Password', dest='password')
+    return argparser.parse_args()
 
 
 
-checkargs()
-suffixes = sys.argv[4].split(',')
+args = handleArgs()
+suffixes = args.fileFilter.split(',')
 
-crawler = SvnCrawler(sys.argv[2],sys.argv[3], suffixes, sys.argv[5])
-if len(sys.argv) == 7:
-  crawler.followLink(sys.argv[1], sys.argv[6])
-else:
-  crawler.followLink(sys.argv[1])
+crawler = SvnCrawler(args.user,args.password, suffixes, args.searchString)
+crawler.followLink(args.baseUrl, args.start)
